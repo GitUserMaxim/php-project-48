b64b691732f0fc04af0d67b9a2664c355c19bbf4
Maxim <raychukmaxim@gmail.com>
2025-01-08 16:49:04 +0300

    Fix Formatter.php

--- src/Formatter.php
+++ src/Formatter.php
@@ -26,13 +26,13 @@ function transformTree(mixed $value, string $replacer = ' ', int $spaceCount = 4
 
         $indentLength = $spaceCount * $depth;
         $shiftToLeft = 2;
-        $indentForImmutableType = str_repeat($replacer, $indentLength);
-        $indentForMutableType = str_repeat($replacer, $indentLength - $shiftToLeft);
+        $indentImmutableType = str_repeat($replacer, $indentLength);
+        $indentMutableType = str_repeat($replacer, $indentLength - $shiftToLeft);
         $bracketIndent = str_repeat($replacer, $indentLength - $spaceCount);
 
         $strings = array_map(
-            function ($item, $key) use ($indentForImmutableType, $indentForMutableType, $iter, $depth) {
-                return formatItem($item, $key, $indentForImmutableType, $indentForMutableType, $iter, $depth);
+            function ($item, $key) use ($indentImmutableType, $indentMutableType, $iter, $depth) {
+                return formatItem($item, $key, $indentImmutableType, $indentMutableType, $iter, $depth);
             },
             $currentValue,
             array_keys($currentValue)
@@ -45,28 +45,28 @@ function transformTree(mixed $value, string $replacer = ' ', int $spaceCount = 4
     return $iter($value, 1);
 }
 
-function formatItem($item, $key, $indentForImmutableType, $indentForMutableType, $iter, $depth)
+function formatItem($item, $key, $indentImmutableType, $indentMutableType, $iter, $depth)
 {
     if (!is_array($item)) {
-        return $indentForImmutableType . $key . ': ' . $iter($item, $depth + 1);
+        return $indentImmutableType . $key . ': ' . $iter($item, $depth + 1);
     }
     if (!array_key_exists('type', $item)) {
-        return $indentForImmutableType . $key . ': ' . $iter($item, $depth + 1);
+        return $indentImmutableType . $key . ': ' . $iter($item, $depth + 1);
     }
 
     switch ($item['type']) {
         case 'added':
-            return $indentForMutableType . '+ ' . $item['key'] . ': ' . $iter($item['value'] ?? 'null', $depth + 1);
+            return $indentMutableType . '+ ' . $item['key'] . ': ' . $iter($item['value'] ?? 'null', $depth + 1);
         case 'deleted':
-            return $indentForMutableType . '- ' . $item['key'] . ': ' . $iter($item['value'] ?? 'null', $depth + 1);
+            return $indentMutableType . '- ' . $item['key'] . ': ' . $iter($item['value'] ?? 'null', $depth + 1);
         case 'changed':
-            return $indentForMutableType . '- ' . $item['key'] . ': ' . $iter($item['valueOld'] ?? 'null', $depth + 1) .
-                "\n" . $indentForMutableType . '+ ' . $item['key'] . ': ' . $iter($item['valueNew'] ?? 'null', $depth + 1);
+            return $indentMutableType . '- ' . $item['key'] . ': ' . $iter($item['valueOld'] ?? 'null', $depth + 1) .
+                "\n" . $indentMutableType . '+ ' . $item['key'] . ': ' . $iter($item['valueNew'] ?? 'null', $depth + 1);
         case 'unchanged':
-            return $indentForImmutableType . $item['key'] . ': ' . $iter($item['value'] ?? 'null', $depth + 1);
+            return $indentImmutableType . $item['key'] . ': ' . $iter($item['value'] ?? 'null', $depth + 1);
         case 'nested':
-            return $indentForImmutableType . $item['key'] . ': ' . $iter($item['children'] ?? [], $depth + 1);
+            return $indentImmutableType . $item['key'] . ': ' . $iter($item['children'] ?? [], $depth + 1);
         default:
-            return $indentForImmutableType . $key . ': ' . $iter($item, $depth + 1);
+            return $indentImmutableType . $key . ': ' . $iter($item, $depth + 1);
     }
 }
